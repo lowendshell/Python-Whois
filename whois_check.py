4527592ab4fddc744875452e421675394790d75c
LowEndShell <78524165+lowendshell@users.noreply.github.com>
2021-11-11 23:41:11 +0800

    Update whois_check.py

--- whois_check.py
+++ whois_check.py
@@ -5,8 +5,8 @@ import re
 
 def get_whois(domain_name):
     try:
-        domain_name_match = re.match(r'[a-zA-Z0-9][-a-zA-Z0-9]{0,62}\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62}', domain_name)
-        domain_name_match_long = re.match(r'[a-zA-Z0-9][-a-zA-Z0-9]{0,62}\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62}\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62}', domain_name)
+        domain_name_match = re.match(r'[a-zA-Z0-9][-a-zA-Z0-9]{0,62}\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62}', domain_name.decode())
+        domain_name_match_long = re.match(r'[a-zA-Z0-9][-a-zA-Z0-9]{0,62}\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62}\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62}', domain_name.decode())
         if domain_name_match == None:
             return 'Input Error \n Please input correct domain name!'
         else:

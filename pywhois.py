cf64381345cdc8b8580a7209030f23ddc5f39ba2
LowEndShell <78524165+lowendshell@users.noreply.github.com>
2021-11-11 23:44:17 +0800

    Update pywhois.py

--- pywhois.py
+++ pywhois.py
@@ -7,12 +7,14 @@ from flask import url_for
 from flask import redirect
 from flask import request
 from whois_check import get_whois
+import importlib
 import sys
 
 
 # fix the problem of chinese
-reload(sys)
-sys.setdefaultencoding('utf-8')
+#reload(sys)
+#sys.setdefaultencoding('utf-8')
+importlib.reload(sys)
 
 
 app = Flask(__name__)
@@ -30,16 +32,16 @@ def about():
 @app.route('/whois', methods=['POST'])
 def whois_check():
     domain_name = request.form['domain_name']
-    dn_info_str = get_whois(domain_name)
-    dn_info = dn_info_str.replace('\n', '<br>')
+    dn_info_str = get_whois(domain_name.encode())
+    dn_info = dn_info_str.decode().replace('\n', '<br>')
     return render_template('whois_info.html', dn_info=dn_info, dn_value=domain_name)
 
 @app.route('/whois/<domain_name>')
 def whois_info(domain_name):
-    dn_info_str = get_whois(domain_name)
-    dn_info = dn_info_str.replace('\n', '<br>')
+    dn_info_str = get_whois(domain_name.encode())
+    dn_info = dn_info_str.decode().replace('\n', '<br>')
     return render_template('whois_info.html', dn_info=dn_info, dn_value=domain_name)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5001)
